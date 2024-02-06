f94d86d51d074cd18a421d0d1dd0748eca619a3b
Pannatuck <nikita.finazhin@gmail.com>
2024-02-06 15:44:40 +0200

    Initial commit. Dependencies, package folders, color/theme, icon.

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript{
+    dependencies{
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+}
