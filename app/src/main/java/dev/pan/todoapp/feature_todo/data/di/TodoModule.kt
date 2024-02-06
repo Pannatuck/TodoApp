6edc8892525086f3be1f3db3cc637f4aa0020395
Pannatuck <nikita.finazhin@gmail.com>
2024-02-06 18:55:04 +0200

    Added Firebase Realtime DB, retrofit setup (api, di, item(data class)), Domain layer model (data class), Util class for future exception handling if needed, Mappers.

--- app/src/main/java/dev/pan/todoapp/feature_todo/data/di/TodoModule.kt
+++ app/src/main/java/dev/pan/todoapp/feature_todo/data/di/TodoModule.kt
@@ -9,11 +9,31 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dev.pan.todoapp.feature_todo.data.local.TodoDao
 import dev.pan.todoapp.feature_todo.data.local.TodoDatabase
+import dev.pan.todoapp.feature_todo.data.remote.TodoApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TodoModule {
+
+    @Provides
+    fun providesRetrofitApi(retrofit: Retrofit): TodoApi {
+        return retrofit.create(TodoApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return Retrofit.Builder().addConverterFactory(
+            GsonConverterFactory.create()
+        )
+            .baseUrl("https://todoapp-70b0f-default-rtdb.firebaseio.com/")
+            .build()
+    }
+
     @Provides
     fun providesRoomDao(database: TodoDatabase): TodoDao {
         return database.dao
---
+++ app/src/main/java/dev/pan/todoapp/feature_todo/domain/util/InvalidTodoItemException.kt
@@ -0,0 +1,3 @@
+package dev.pan.todoapp.feature_todo.domain.util
+
+class InvalidTodoItemException(message: String): Exception(message)
